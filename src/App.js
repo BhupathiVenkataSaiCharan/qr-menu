aadc4cc14a835b61b1c75941b9e698a04dff2d42
Bhupathi Venkata Sai Charan <venkatasaicharanbhupathi@gmail.com>
2023-01-09 14:12:08 +0530

    laptopscreenloading

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import { BrowserRouter as Router,Routes,Route, Navigate } from 'react-router-dom
 
 function App() {
   return (
-    // <div className="App">
-    //   <Navbar/>
-    // </div>
     <div className='App'>
       <Router>
         <Routes>
